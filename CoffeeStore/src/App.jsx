889af8d40c944e361a20d23a154115664a330267
Teri Chudějová <tereza.chudejova@gmail.com>
2024-07-30 11:12:37 +0200

    create a landing page

--- CoffeeStore/src/App.jsx
+++ CoffeeStore/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import "./mainContainer.css"
+import "./landingPage.css"
 
 function App() {
 
   return (
-        <section id='mainContainer'>
-        </section>
+    <div>
+        <div id='mainContainer'>
+            <div id='landingPage'>
+
+            </div>
+        </div>
+    </div>
   )
 }
 
