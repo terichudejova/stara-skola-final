c2556f9479ff6ecf3dac9278e21f2cdc0c3f7559
Teri Chudějová <tereza.chudejova@gmail.com>
2024-07-30 13:24:19 +0200

    try to merge files

--- CoffeeStore/src/App.jsx
+++ CoffeeStore/src/App.jsx
@@ -4,10 +4,6 @@ import "./mainContainer.css"
 import "./landingPage.css"
 import NavigationBar from './NavigationBar'
 import "./navbar.css"
-<<<<<<< HEAD
-=======
-import "./nabidka.css"
->>>>>>> nabidka
 
 function App() {
 
@@ -16,18 +12,12 @@ function App() {
         <div id='mainContainer'>
             <div id='landingPage'>
               <div id='navbar'>
-<<<<<<< HEAD
-                <NavigationBar/>
-              </div>
-                <h1>kavárna Stará škola</h1>
-=======
                   <NavigationBar/>
               </div>
                 <h1>kavárna Stará škola</h1>
             </div>
             <div id='nabidka'>
 
->>>>>>> nabidka
             </div>
         </div>
 f    </div>
