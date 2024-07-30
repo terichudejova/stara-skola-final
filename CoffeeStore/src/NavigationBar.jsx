e920b3fe585030805ceb4eccd81daf309837a8c6
Teri Chudějová <tereza.chudejova@gmail.com>
2024-07-30 13:10:35 +0200

    add hamburger menu to navbar

---
+++ CoffeeStore/src/NavigationBar.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./NavigationBar.css"
+
+export default function NavigationBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="hamburger-menu">
+      <div className={`menu-toggle ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <nav className={`nav-menu ${isOpen ? 'open' : ''}`}>
+        <a href="#onas">O nás</a>
+        <a href="#nabidka">Nabídka</a>
+        <a href="#kontakt">Kontakt</a>
+      </nav>
+    </div>
+  );
+}
